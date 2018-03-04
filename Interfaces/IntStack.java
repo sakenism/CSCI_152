4fa176f9d2293f3d0b08b5bb66e90c79042b71cc
sakenism <saken.mukanov@nu.edu.kz>
2018-03-04 20:40:11 +0600

    Slight change of Interfaces

--- Interfaces/IntStack.java
+++ Interfaces/IntStack.java
@@ -1,4 +1,4 @@
-package Interfaces;
+//package Interfaces;
 
 public interface IntStack
 {
--- Interfaces/Queue.java
+++ Interfaces/Queue.java
@@ -4,7 +4,7 @@
  * and open the template in the editor.
  */
 
-package Interfaces;
+//package Interfaces;
 /**
  *
  * @param <T>
